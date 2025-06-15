a88e7b41f133959618fe3d290085ea097674194c
thePJPL <thepjpl@hotmail.com>
2025-06-14 22:44:39 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
+hamburger.addEventListener('click', () => navLinks.classList.toggle('active'));
